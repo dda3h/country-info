11eb02a0f2d597ff2ec792e46cd29bd53a17daa6
dda3h <125443406+dda3h@users.noreply.github.com>
2024-06-07 13:37:41 +0300

    add loading animation

--- src/components/LoadingStatusHandler/index.tsx
+++ src/components/LoadingStatusHandler/index.tsx
@@ -17,7 +17,22 @@ const LoadingStatusHandler = ({ isLoading, error, children }: Props) => {
 		);
 	}
 
-	if (isLoading) return <h1 className={styles.header}>Loading...</h1>;
+	if (isLoading) {
+		return (
+			<svg viewBox='0 0 50 50' className={styles.spinner}>
+				<circle
+					cx='25'
+					cy='25'
+					r='20'
+					fill='none'
+					stroke='currentColor'
+					strokeLinecap='round'
+					strokeWidth='3'
+					className={styles.spinner__path}
+				/>
+			</svg>
+		);
+	}
 
 	return children;
 };
